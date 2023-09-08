1d5bd6744f9e90a7953b3d6a442d57e5f53f15fb
fmaring04 <f.maring04@gmail.com>
2023-09-08 01:13:00 -0300

    1.1
    Creación página Recover

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,11 @@ const routes: Routes = [
   {
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },  {
+    path: 'recover',
+    loadChildren: () => import('./recover/recover.module').then( m => m.RecoverPageModule)
   },
+
 
 ];
 
