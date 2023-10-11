11e4054658c45701c3145f44a2f493ea1ae5b9d4
fmaring04 <f.maring04@gmail.com>
2023-10-10 23:04:46 -0300

    Update home.page.ts

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router, NavigationExtras, ActivatedRoute } from '@angular/router';
 
 @Component({
