dcebd30c5d7b94cad2276bcef45678a523f02704
fmaring04 <f.maring04@gmail.com>
2023-10-10 22:55:47 -0300

    10-10
    Se aplicó navigationextras para recibir el usuario

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { Router, NavigationExtras, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
+  
+  public user = {
+    usuario: "",
+    pass: ""
+  }
 
-  constructor() {}
+  constructor(private router: Router, private activeRoute: ActivatedRoute) {
+
+  }
+
+  ngOnInit() {
+    this.activeRoute.queryParams.subscribe(() => {
+      let state = this.router.getCurrentNavigation()?.extras.state;
+      if (state) {
+        this.user.usuario = state['user'].usuario;
+        this.user.pass = state['user'].pass;
+        console.log(this.user);
+      }
+    })
+  }
 
 }
