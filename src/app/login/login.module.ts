7b0a82a02ef88d157294ac66a87df1936a46851b
Playdbunny <solops400@gmail.com>
2023-10-20 02:30:07 -0300

    a

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -8,13 +8,17 @@ import { LoginPageRoutingModule } from './login-routing.module';
 
 import { LoginPage } from './login.page';
 
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    LoginPageRoutingModule
+    LoginPageRoutingModule,
+    MatProgressBarModule
   ],
   declarations: [LoginPage]
+    
 })
 export class LoginPageModule {}
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router, NavigationExtras } from '@angular/router';
-
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
+  
 })
 export class LoginPage implements OnInit {
 
