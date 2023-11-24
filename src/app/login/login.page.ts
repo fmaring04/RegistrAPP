31ac7d44cd5d4c708abe96c82f1e2b2252114ece
fmaring04 <f.maring04@gmail.com>
2023-11-24 03:49:03 -0300

    .

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router, NavigationExtras } from '@angular/router';
+import { MydbService } from '../service/mydb.service';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -13,16 +15,20 @@ export class LoginPage implements OnInit {
     pass: ""
   }
 
-  constructor(private router:Router, private activeRouter:ActivatedRoute) { }
+  constructor(private router:Router, private activeRouter:ActivatedRoute, private mydb:MydbService) { }
 
   enviarPerfil() {
     const navigationExtras: NavigationExtras = {
       state: { user: this.user}
     }
+
+    localStorage.setItem('usuario', this.user.usuario);
+
     this.router.navigate(['/home'], navigationExtras);
   }
   
   ngOnInit() {
   }
+
 
 }
