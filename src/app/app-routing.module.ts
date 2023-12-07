0aefed065c20de1408574958ca4854be6386fb64
Playdbunny <solops400@gmail.com>
2023-12-07 00:10:42 -0300

    a
    a

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,24 +7,28 @@ const routes: Routes = [
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
+
   {
     path: '',
     redirectTo: 'login',
     pathMatch: 'full'
   },
+
   {
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   },
+
   {
     path: 'recover',
     loadChildren: () => import('./recover/recover.module').then( m => m.RecoverPageModule) 
-  },  {
+  },
+  
+  {
     path: 'docente',
     loadChildren: () => import('./docente/docente.module').then( m => m.DocentePageModule)
   },
-
-
+  
 
 ];
 
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,21 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router, NavigationExtras, ActivatedRoute } from '@angular/router';
+import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage implements OnInit{
+export class HomePage implements OnInit, OnDestroy{
+
+  qrCodeString= "this is a secret QRcode message";
+  scannedResult: any;
+  content_visibility = "";
   
   public user = {
     usuario: "",
     pass: ""
   }
 
-  constructor(private router: Router, private activeRoute: ActivatedRoute) {
-
-  }
+  constructor(private router: Router, private activeRoute: ActivatedRoute) {}
 
   ngOnInit() {
     this.activeRoute.queryParams.subscribe(() => {
@@ -27,5 +30,57 @@ export class HomePage implements OnInit{
       }
     })
   }
+
+  async checkPermission() {
+    try {
+      // check or request permission
+      const status = await BarcodeScanner.checkPermission({ force: true });
+      if (status.granted) {
+        // the user granted permission
+        return true;
+      }
+      // the user denied permission
+      return false;
+    } catch (e) {
+      console.log(e);
+      return false;
+    }
+  }
+
+  async startScan() {
+    try{
+      const permissions = await this.checkPermission();
+      if (!permissions) {
+        return;
+      }
+      await BarcodeScanner.hideBackground(); // make background of WebView transparent
+      document.querySelector('body')?.classList.add('scanner-active');
+      this.content_visibility = 'hidden';
+      const result = await BarcodeScanner.startScan(); // start scanning and wait for a result
+      console.log(result);
+      
+      BarcodeScanner.showBackground();
+      document.querySelector('body')?.classList.remove('scanner-active');
+      this.content_visibility = '';
+      if (result?.hasContent) {
+        this.scannedResult = result.content;
+        console.log(this.scannedResult);
+      }
+    }catch(e){
+      console.log(e);
+      this.stopScan();
+    }
+  }
+  stopScan() {
+    BarcodeScanner.showBackground();
+    BarcodeScanner.stopScan();
+    document.querySelector('body')?.classList.remove('scanner-active');
+    this.content_visibility = '';
+  }
+
+  ngOnDestroy(): void {
+    this.stopScan();
+  }
+
 
 }
